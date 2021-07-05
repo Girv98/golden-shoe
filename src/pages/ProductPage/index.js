3a537995dd4ce08f9c0fb860f430e6f887476c80
James Girven <james.girven10@gmail.com>
2021-07-05 23:29:19 +0100

    Changed button logic Fixed Product Page

--- src/pages/ProductPage/index.js
+++ src/pages/ProductPage/index.js
@@ -61,25 +61,48 @@ const ProductPage = props => {
     //     history.push('/cart');
     //   }
 
-    console.log(product)
+    //console.log(product)
 
     return (
-        <section className="product-wrapper">
-          <div className="image-wrapper">
-            <img className="product-image" src={product.productImage} alt="" />
-          </div>
-          <div className="product-info">
-            <h1 className="product-name">{product.productName}</h1>
-            <h3>£{product.productPrice}</h3>
+        <section className="wrapper--product">
 
-            <div className="input-wrapper">   
-              <button>sizeguide</button>
+          <div id="open-modal" className="modal">
+            <div>
+              <a href="#" title="Close" className="modal__close">Close</a>
+              <h1>Size Guide</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum harum pariatur voluptas fugiat magnam, ratione fuga laboriosam, inventore nulla porro delectus, in necessitatibus beatae? Voluptate.</p>
+            </div>
+          </div>
+
+
+          <div className="wrapper--image">
+            <img className="product--image" src={product.productImage} alt="" />
+          </div>
+          
+          <div className="product--info">
+            <h1 className="product--name">{product.productName}</h1>
+            <h3 className="product--price">£{product.productPrice}</h3>
+          </div>
+
+          <div className="product--options">
+            <div className="wrapper--size"> 
+              <div className="size__title">
+                  <h4 className="title--left">Select Size</h4>  
+                  <a className="title--right" href="#open-modal">sizeguide</a>
+              </div>
               <SizeButtons size={size} setSize={setSize} />
-              
             </div>
 
-            <button className={`button add--button ${buyable ? "is--active" : "is--not-active"}`} disabled={!buyable}>
-              {buyable ? "Add to basket" : "Please choose available size"}
+            <button className={`button add--button ${buyable ? "is--active" : "is--not-active"}`} disabled={!buyable} title={`${buyable ? "Add to bag" : "Please choose available size"}`}>
+              {buyable &&
+              "Add to Bag" 
+              }
+              {!buyable && size !== "a" &&
+              "Not available in this size" 
+              }
+              { size === "a" &&
+                "Please choose a size"
+              }
             </button>
           </div>
         </section>
